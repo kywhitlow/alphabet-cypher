24ff06197895dfc2256dbf17568801e1ccf8012c
Daniel McMullen <daniel.mcmullen@gmail.com>
2019-08-29 22:53:46 -0400

    basic setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const Cypher = {
+  encode: (key, message) => {
+  },
+  
+  decode: (key, encodedMessage) => {
+  },
+  
+  decypher: (encodedMessage, message) => {
+  }
+}
+
+export default Cypher
---
+++ src/index.spec.js
@@ -0,0 +1,63 @@
+import Cypher from './index'
+
+describe('Alphabet Cypher', () => {
+  describe('Encode', () => {
+    it('can encode a message with vigilance as a secret keyword', () => {
+      const secretKeyword = 'vigilance'
+      const message = 'meetmeontuesdayeveningatseven'
+
+      const encodedMessage = Cypher.encode(secretKeyword, message)
+
+      expect(encodedMessage).toBe('hmkbxebpxpmyllyrxiiqtoltfgzzv')
+    });
+    
+    it('can encode a message with scones as a secret keyword', () => {
+      const secretKeyword = 'scones'
+      const message = 'meetmebythetree'
+
+      const encodedMessage = Cypher.encode(secretKeyword, message)
+
+      expect(encodedMessage).toBe('egsgqwtahuiljgs')
+    });
+  });
+
+  describe('Decode', () => {
+    it('can decode a message with vigilance as a secret keyword', () => {
+      const secretKeyword = 'vigilance'
+      const encodedMessage = 'hmkbxebpxpmyllyrxiiqtoltfgzzv'
+
+      const message = Cypher.decode(secretKeyword, encodedMessage)
+
+      expect(message).toBe('meetmeontuesdayeveningatseven')
+    });
+    
+    it('can decode a message with scones as a secret keyword', () => {
+      const secretKeyword = 'scones'
+      const encodedMessage = 'egsgqwtahuiljgs'
+
+      const message = Cypher.decode(secretKeyword, encodedMessage)
+
+      expect(message).toBe('meetmebythetree')
+    });
+  });
+  
+  describe('Decypher', () => {
+    it('can extract the secret keyword given an encrypted message and the original message 1', () => {
+      const message = 'meetmeontuesdayeveningatseven'
+      const encodedMessage = 'hmkbxebpxpmyllyrxiiqtoltfgzzv'
+
+      const secretKeyword = Cypher.decypher(encodedMessage, message)
+
+      expect(secretKeyword).toBe('vigilance')
+    });
+    
+    it('can extract the secret keyword given an encrypted message and the original message 2', () => {
+      const message = 'meetmebythetree'
+      const encodedMessage = 'egsgqwtahuiljgs'
+
+      const secretKeyword = Cypher.decypher(encodedMessage, message)
+
+      expect(secretKeyword).toBe('scones')
+    });
+  });
+});
